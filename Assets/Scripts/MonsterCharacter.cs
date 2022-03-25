a01ac20222f3292b75d667c0013085dbe0f013ae
Stephanie Dang <stphnie.dang@gmail.com>
2022-03-25 12:25:33 -0400

    P3: Added Options and SFX
    Player can now choose to take pills or not

--- Assets/Scripts/MonsterCharacter.cs
+++ Assets/Scripts/MonsterCharacter.cs
@@ -9,16 +9,17 @@ public class MonsterCharacter : MonoBehaviour
     public NPCConversation myConvo;
     public GameObject ConvoBox;
     public Vector3 newPos;
-    
+    GameObject player;
 
     void Start() {
-        
+        player = GameObject.Find("punpun");
     }
     private void OnMouseOver() {
         if(Input.GetMouseButtonDown(0)){
             ConversationManager.Instance.StartConversation(myConvo);
             RectTransform rt = ConvoBox.GetComponent<RectTransform>();
             rt.localPosition = new Vector3(newPos.x,newPos.y,newPos.z);
+            player.GetComponent<Move>().speed =0f;
         }
     }
 }
--- Assets/Scripts/TakePills.cs
+++ Assets/Scripts/TakePills.cs
@@ -2,31 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DialogueEditor;
 
 public class TakePills : MonoBehaviour
 {
     int currentCount = 0;
+    public NPCConversation myConvo;
+    public GameObject ConvoBox;
     public GameObject Pills;
-    public Button yourButton;
+    public Button noButton, yesButton;
+    GameObject player;
+    Rigidbody rigidB;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-        Invoke("AskQuestion",10f);
+        player = GameObject.Find("punpun");
+        noButton.onClick.AddListener(noBtnTask);
+        yesButton.onClick.AddListener(yesBtnTask);
+        Invoke("AskQuestion",7f);
     }
 
     void TaskOnClick() {
         currentCount++;
         Pills.gameObject.SetActive(false);
-        Invoke("AskQuestion",4f);
+        Invoke("AskQuestion",5f);
     }
 
     void AskQuestion() {
         if(currentCount < 4) {
             Pills.gameObject.SetActive(true);
         }
+    }
+
+    void noBtnTask() {
+        currentCount++;
+        Pills.gameObject.SetActive(false);
+        Invoke("AskQuestion",5f);
+    }
+
+    public void yesBtnTask() {
+        ConversationManager.Instance.StartConversation(myConvo);
+        RectTransform rt = ConvoBox.GetComponent<RectTransform>();
+        rt.localPosition = new Vector3(player.transform.position.x,0f,player.transform.position.x);
+        currentCount++;
+        Pills.gameObject.SetActive(false);
+        player.GetComponent<Move>().speed = 0f;
+        Invoke("AskQuestion",10f);
+        
+    }
+
+    public void ResetSpeed() {
+        player.GetComponent<Move>().speed = 5f;
     }
 
 }
